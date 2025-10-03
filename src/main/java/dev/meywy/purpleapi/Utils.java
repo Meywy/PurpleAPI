a235ff2b61f8494a4cc4978e417904e301cf0fc0
meywy <meywyy@gmail.com>
2025-10-03 22:32:52 +0200

    Make colorize method static

--- src/main/java/dev/meywy/purpleapi/Utils.java
+++ src/main/java/dev/meywy/purpleapi/Utils.java
@@ -2,7 +2,7 @@ package dev.meywy.purpleapi;
 
 import org.bukkit.ChatColor;
 
-public abstract class Utils {
+public class Utils {
 
     public String colorize(String message) {
         return ChatColor.translateAlternateColorCodes('&', message);
