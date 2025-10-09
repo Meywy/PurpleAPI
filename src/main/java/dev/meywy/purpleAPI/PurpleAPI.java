0058d5d8826e61df884427b01c08238261aac995
meywy <meywyy@gmail.com>
2025-10-09 17:57:58 +0200

    Initial commit

---
+++ src/main/java/dev/meywy/purpleAPI/PurpleAPI.java
@@ -0,0 +1,17 @@
+package dev.meywy.purpleAPI;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PurpleAPI extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
